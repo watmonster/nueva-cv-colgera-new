9c81a10232de72e2bc9febe486a77c0552739e53
watmonster <112777372+watmonster@users.noreply.github.com>
2025-11-13 20:20:54 -0800

    added basic functions, uploaded image

--- my_project/example_module.py
+++
@@ -1,2 +0,0 @@
-def a_function_from_another_module():
-    print("And now I am running a function from another module")
--- my_project/hello_world.py
+++ my_project/hello_world.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python
 
+# from example_module import a_function_from_another_module
+
+from skimage import io
+from skimage import color
 import numpy as np
+import matplotlib.pyplot as plt
 
-from example_module import a_function_from_another_module
-
-
-def a_local_function():
-    print("Coming to you now from a local function")
-    print("I can run numpy commands!")
-    print(np.array([1, 2, 3]) * 2)
+# from time import time
+import cv2
 
 
-def main_function():
-    print("Hello, World!")
-    a_local_function()
-    a_function_from_another_module()
+# Make matplotlib figures appear inline in the notebook rather than in a new window
+# %matplotlib inline
+plt.rcParams["figure.figsize"] = (10.0, 8.0)  # set default size of plots
+plt.rcParams["image.interpolation"] = "nearest"
+plt.rcParams["image.cmap"] = "gray"
 
 
-if __name__ == "__main__":
-    main_function()
+def load(image_path):
+    out = io.imread(image_path)
+    out = out.astype(np.float64) / 255
+    return out
+
+
+def display(img):
+    # Show image
+    plt.figure(figsize=(5, 5))
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
+
+
+image_path = "./my_project/column_0011.png"
+image = load(image_path)
+display(image)
+
+
+def corner_detection(img):
+    gray_img = color.rgb2gray(img)
+    corners = cv2.cornerHarris(gray_img, 4, 3, 0.04)
+    corners = cv2.dilate(corners, None)
+    image[corners > 0.01 * corners.max()] = [0, 0, 255]
+    # img_plus_corners = np.add(gray_img)
+    cv2.imshow("corners??", gray_img)
+
+
+corner_detection(image)
